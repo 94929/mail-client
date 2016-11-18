72f20e3d180c7841855a960b5a20c0a7efd03fae
jsung5381 <jsung5381@naver.com>
2016-11-18 22:05:12 +0900

    Opened a client socket to the mail server and set the basic implementation of message sender and receiver

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,7 @@
-
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
 
 /**
  * Created by jsh3571 on 14/11/2016.
@@ -7,6 +10,28 @@
 public class Main {
 
     public static void main(String[] args) {
-        // Git tests
+        try {
+            Socket client = new Socket("mail.comtrue.com", 110);
+
+            BufferedReader sender
+                    = new BufferedReader(new InputStreamReader(System.in));
+            BufferedReader receiver
+                    = new BufferedReader(
+                            new InputStreamReader(client.getInputStream()));
+
+            while (true) {
+                System.out.println("input the data u want to send to echo server: ");
+                String s = sender.readLine();
+
+                if (s.equals("exit"))
+                    System.exit(1);
+
+                String st = receiver.readLine();
+                System.out.println("data returned by the server:   ");
+                System.out.println(st);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
