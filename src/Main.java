9f02090de3e5c71043b31a6e0f5da7a77270dda5
jsung5381 <jsung5381@naver.com>
2016-11-19 10:03:44 +0900

    Refactored object names to improve clarity

--- src/Main.java
+++ src/Main.java
@@ -14,13 +14,14 @@ public class Main {
         try {
             /* Input format checking before opening a connection,
                 the input must be a form of mail_server and port_number */
-            if (args.length != 2) System.exit(1);
+            if (args.length != 2)
+                System.exit(1);
 
             /* Set up the connection to mail server from the command line */
             client = new Socket(args[0], Integer.parseInt(args[1]));
 
-            /* Initialize 'ps' that sends message to the server */
-            PrintStream ps = new PrintStream(client.getOutputStream());
+            /* Initialize sender that sends message to the server */
+            PrintStream sender = new PrintStream(client.getOutputStream());
 
             /* Initialize receiver that receives message from the server */
             BufferedReader receiver
@@ -28,18 +29,20 @@ public class Main {
                             new InputStreamReader(client.getInputStream()));
 
             while (true) {
+                /* Flushing buffer */
+                String message;
+
                 /* Printing the resulting response from the server */
                 System.out.println(receiver.readLine());
 
                 /* Get input message from the user */
-                String message = br.readLine();
+                message = br.readLine();
 
                 /* If the user inputs "exit", then the program terminates */
-                if (message.equals("exit"))
-                    System.exit(1);
+                if (message.equals("exit")) System.exit(1);
 
                 /* If not exit, send the message to server */
-                ps.println(message);
+                sender.println(message);
             }
         } catch (Exception e) {
             e.printStackTrace();
